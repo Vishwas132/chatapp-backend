453573a2debe45b63fcb6e75893f1ec737c4ed13
Vishwas Lekhwar <64309462+Vishwas132@users.noreply.github.com>
2024-11-28 19:34:54 +0530

    Update WebSocket middleware to use ES modules

--- src/middlewares/websocket.js
+++ src/middlewares/websocket.js
@@ -1,27 +1,29 @@
-module.exports = (config, { strapi }) => {
-  const io = require('socket.io')(strapi.server.httpServer, {
-    cors: {
-      origin: "http://localhost:5173",
-      methods: ["GET", "POST"]
-    }
-  });
+import { Server } from 'socket.io';
 
-  io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    socket.on('message', (message) => {
-      // Echo the message back to the client
-      socket.emit('message', message);
+export default ({ strapi }) => {
+  strapi.server.httpServer.once('listening', () => {
+    const io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+      }
     });
 
-    socket.on('disconnect', () => {
-      console.log('User disconnected');
+    io.on('connection', (socket) => {
+      console.log('A user connected');
+
+      socket.on('message', (message) => {
+        // Echo the message back to the client
+        socket.emit('message', message);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('User disconnected');
+      });
     });
+
+    strapi.io = io;
   });
 
-  return {
-    initialize() {
-      strapi.io = io;
-    },
-  };
+  return () => {};
 };
